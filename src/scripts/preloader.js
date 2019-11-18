adc3a0a0b66a63c69c0d435e07fc67a99abb0cdf
Sergey Antonov <flyker777@yandex.ru>
2019-11-18 15:52:48 +0300

    Initial dist subtree commit

--- src/scripts/preloader.js
+++
@@ -1,6 +0,0 @@
-const preloader = document.querySelector('.preloader');
-
-window.addEventListener("load", () => {  
-  preloader.style.display = 'none';
-  document.body.style.overflow = 'auto';
-});
